69c61ad85b45a93e721d8eb8781d01007086af23
Erik Gafni <egafni@gmail.com>
2012-12-06 19:27:40 -0500

    update default config

--- config/default.py
+++ config/default.py
@@ -1,47 +1,34 @@
-import os
+import os 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'cosmos.Cosmos.django_settings'
 os.environ['COSMOS_HOME_PATH'] = '/home/erik/workspace/cosmos'
 
+#######################
+# Cosmos
+#######################
 
-# Cosmos Settings
-home_path = '/home/erik/workspace/Cosmos' #gpp
-default_root_output_dir = '/tmp/cosmos_out' #gpp
+home_path = os.environ['COSMOS_HOME_PATH']
+default_root_output_dir = '/tmp/cosmos_out' # The directory to output all files to
 DRM = 'GE' #LSF, or GE
 tmp_dir = '/tmp'
 
-# Web interface settings
-show_stage_file_sizes = False # on some systems, file i/o slows down a lot when running a lot of jobs making this feature very slow
+########################
+# Web interface
+########################
+
+# on some systems, file i/o slows down a lot when running a lot of jobs making file_size calculations very slow.
+show_stage_file_sizes = False 
 show_jobAttempt_file_sizes = False
 show_task_file_sizes = False
+
+########################
+# Database
+########################
 
 DATABASE = {
     'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
     'NAME': '/home/erik/workspace/Cosmos/sqlite.db',                      # Or path to database file if using sqlite3.
-#    'USER': 'cosmos',                      # Not used with sqlite3.
-#    'PASSWORD': '12345erik',                  # Not used with sqlite3.
-#    'HOST': 'GP-DB.tch.harvard.edu',                      # Set to empty string for localhost. Not used with sqlite3.
-#    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    'USER': '',                      # Not used with sqlite3.
+    'PASSWORD': '',                  # Not used with sqlite3.
+    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
 }
-
-
-#import os
-#
-## Cosmos Settings
-#home_path = '/home/ch158749/workspace/Cosmos' #gpp
-#default_root_output_dir = '/nas/erik/cosmos_output2' #gpp
-#DRM = 'LSF' #LSF, or GE
-#tmp_dir = '/nas/erik/tmp'
-#
-## Web interface settings
-#show_stage_file_sizes = False # on some systems, file i/o slows down a lot when running a lot of jobs making this feature very slow
-#show_jobAttempt_file_sizes = False
-#show_task_file_sizes = False
-#
-#DATABASE = {
-#    'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#    'NAME': 'Cosmos',                      # Or path to database file if using sqlite3.
-#    'USER': 'cosmos',                      # Not used with sqlite3.
-#    'PASSWORD': '12345erik',                  # Not used with sqlite3.
-#    'HOST': 'GP-DB.tch.harvard.edu',                      # Set to empty string for localhost. Not used with sqlite3.
-#    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#}
