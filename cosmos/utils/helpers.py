49bea2aec45f53891ede4b3669a7d83157976752
Ubuntu <ubuntu@master.(none)>
2013-10-21 15:06:47 +0000

    update parse_cmd in /utils/helpers.py to allow mutilple-commands in one script

--- cosmos/utils/helpers.py
+++ cosmos/utils/helpers.py
@@ -117,6 +117,7 @@ def parse_cmd(txt,**kwargs):
         x = map(lambda x: re.sub(r"\\$",'',x.strip()).strip(),x)
         x = filter(lambda x: not x == '',x)
         x = ' \\\n'.join(x)
+        x = re.sub(';[ ]*\\\\','\n',x)   # remove backslash from comma-ended string by Jae 10/21/2013
     except (KeyError,TypeError):
         formatError(txt,kwargs)
     return x
