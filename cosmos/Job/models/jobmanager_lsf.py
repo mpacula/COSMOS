c617b4d3d70b327c28bcefc41437b22728264ae0
JaeYJung <Jae-Yoon_Jung@hms.harvard.edu>
2014-03-20 18:32:16 -0400

    Update Popen.wait() to Popen.commnunicate()

--- cosmos/Job/models/jobmanager_lsf.py
+++ cosmos/Job/models/jobmanager_lsf.py
@@ -1,8 +1,9 @@
+import os,re
+import shlex,subprocess
+
 from jobattempt import JobAttempt
 from jobmanager import JobManagerBase
-from subprocess import Popen, PIPE
-import os
-import re
+from subprocess import Popen, PIPE, check_output
 
 class JobStatusError(Exception):
     pass
@@ -12,13 +13,13 @@ current_jobs = []
 
 decode_lsf_state = dict([
     ('UNKWN', 'process status cannot be determined'),
-    ('PEND', 'job is queued and active'),
+    ('PEND',  'job is queued and active'),
     ('PSUSP', 'job suspended while pending'),
-    ('RUN', 'job is running'),
+    ('RUN',   'job is running'),
     ('SSUSP', 'job is system suspended'),
     ('USUSP', 'job is user suspended'),
-    ('DONE', 'job finished normally'),
-    ('EXIT', 'job finished, but failed'),
+    ('DONE',  'job finished normally'),
+    ('EXIT',  'job finished, but failed'),
     ])
 
 def preexec_function():
@@ -27,56 +28,69 @@ def preexec_function():
     # terminate jobs when there is a ctrl+c event
     os.setpgrp()
 
-def get_bjobs():
-    """
-    returns a dict keyed by lsf job ids, who's values are a dict of bjob
-    information about the job
-    """
-    p = Popen(['bjobs','-a'],stdout=PIPE)
-    p.wait()
-    lines = p.stdout.readlines()
-    bjobs = {}
-    header = re.split("\s\s+",lines[0])
-    for l in lines[1:]:
-        items = re.split("\s\s+",l)
-        bjobs[items[0]] = dict(zip(header,items))
-    return bjobs
+# def _get_bjobs_stat():
+#     """
+#     returns a dict keyed by lsf job ids, who's values are a dict of bjob
+#     information about the job
+#     """
 
 class JobManager(JobManagerBase):
     """
     Note there can only be one of these instantiated at a time
-    """
-    
+    """   
     class Meta:
         app_label = 'Job'
         db_table  = 'Job_jobmanager'
 
+    def __init__(self,*args,**kwargs):
+        super(JobManager,self).__init__(*args,**kwargs)
+
+
     ## Override JobManager.submit_job::_run_job() at jobmanager.py
     def _run_job(self,jobAttempt):
-        bsub = 'bsub -o {stdout} -e {stderr} {ns}'.format(
-            stdout=jobAttempt.STDOUT_filepath,
-            stderr=jobAttempt.STDERR_filepath,
-            ns=jobAttempt.drmaa_native_specification)
-        p = Popen((bsub + self._create_cmd_str(jobAttempt)).split(' '),
-                  stdout=PIPE,
-                  env=os.environ,
-                  stderr=PIPE,
-                  preexec_fn=preexec_function())
-        p.wait()
-        lsf_id=re.search('Job <(\d+)>',p.stdout.read()).group(1)
+
+        cmd  = self._create_cmd_str(jobAttempt)
+        out  = jobAttempt.STDOUT_filepath
+        err  = jobAttempt.STDERR_filepath
+        spec = jobAttempt.drmaa_native_specification
+
+        bsub = 'bsub -o {0} -e {1} {2} {3}'.format(out,err,spec,cmd)
+
+        ## Make sure stdout/err file exist here
+        open(out,'a').close()
+        open(err,'a').close()
+        
+        p = Popen(shlex.split(bsub), stdout=PIPE, env=os.environ, stderr=PIPE, preexec_fn=preexec_function())
+        #p.wait()
+        (stdoutdata, stderrdata) = p.communicate()
+        
+        lsf_id = re.search('Job <(\d+)>', stdoutdata).group(1)  # "Job <######> is submiited to queue <-------->"
+
         jobAttempt.drmaa_jobID = lsf_id
         current_jobs.append(lsf_id)
         all_processes.append(lsf_id)
 
     def _check_for_finished_job(self):
-        bjobs = get_bjobs()
-        for id in current_jobs:
-            status = bjobs[id]['STAT']
-            if status in ['DONE','EXIT','UNKWN','ZOMBI']:
+
+        # Get (id,stat) from 'bjobs -a' command
+        cmd1 = "bjobs -a"
+        cmd2 = "awk 'NR>1{print $1,$3}'" # get jobid, stat for the current user
+
+        p1  = Popen(shlex.split(cmd1),stdout=PIPE);
+        out = subprocess.check_output(shlex.split(cmd2),stdin=p1.stdout)
+        p1.communicate()
+
+        # change (id, stat) lists into dict
+        bjobs_stat =  dict(map(lambda x: x.split(' '), filter(None, out.split('\n'))))
+
+        #for id in current_jobs:  # current_jobs may have (yet) unlisted job ids
+        for id in bjobs_stat:
+            status = bjobs_stat[str(id)]
+            if id in current_jobs and status in ['DONE','EXIT','UNKWN','ZOMBI']:
                 current_jobs.remove(id)
                 ja = JobAttempt.objects.get(drmaa_jobID=id)
-                successful = True if status == 'DONE' else False
-                ja._hasFinished(successful,bjobs[id])
+                succ = True if status == 'DONE' else False
+                ja._hasFinished(succ,status)
                 return ja
         return None
 
