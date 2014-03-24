c731695e1a2d581b9d1ead23c60410c26048f6c2
Alexander K Lancaster <alex_lancaster@hms.harvard.edu>
2014-03-24 14:28:00 -0400

    Update Django to 1.6.2 for Python < 2.7 as well. Fix syntax error (make if else into separate block).

--- setup.py
+++ setup.py
@@ -20,6 +20,48 @@ print >> sys.stderr, "Installing userfiles to ~/.cosmos"
 
 example_workflows = map(lambda x:os.path.join('example_workflows/',x),filter(lambda x:x[-3:]=='.py',os.listdir('example_workflows')))
 
+if sys.version_info[:2] >= (2,7):	
+    install_requires=[
+             'distribute>=0.6.28',
+             'Django==1.6.2',
+             'MySQL-python==1.2.5',
+             'argparse==1.1',
+             'configobj==4.7.2',
+             'decorator==3.4.0',
+             'django-extensions==1.3.3',
+             'django-picklefield==0.3.1',
+             'docutils==0.10',
+             'drmaa==0.7.6',
+             'ipython==0.13.1',
+             'networkx==1.7',
+             'pygraphviz==1.1',
+             'six==1.2.0',
+             'wsgiref==0.1.2',
+             'ordereddict',
+             'ipdb==0.8'
+     ]
+else:
+    install_requires=[
+             'distribute>=0.6.28',
+             'Django==1.6.2',
+             'MySQL-python==1.2.4',
+             'argparse==1.2.1',
+             'configobj==4.7.2',
+             'decorator==3.4.0',
+             'django-extensions==1.0.3',
+             'django-picklefield==0.3.0',
+             'docutils==0.10',
+             'drmaa==0.5',
+             'ipython==0.13.1',
+             'networkx==1.7',
+             'pygraphviz==1.1',
+             'six==1.2.0',
+             'wsgiref==0.1.2',
+             'ordereddict',
+             'ipdb==0.8'
+   ]
+
+
 setup(name='cosmos',
     version=__version__,
     description = "Workflow Manager",
@@ -30,45 +72,5 @@ setup(name='cosmos',
     scripts=['bin/cosmos'],
     package_data={'cosmos':['default_config.ini','lsf_drmaa.conf']+all_files('cosmos/static')+all_files('cosmos/templates')},
     data_files=[(examples_installation_dir,example_workflows)],
-    
-    if sys.version_info[:2] >= (2,7):	
-	install_requires=[
-      		 'distribute>=0.6.28',
-      		 'Django==1.6.2',
-      		 'MySQL-python==1.2.5',
-      		 'argparse==1.1',
-      		 'configobj==4.7.2',
-      		 'decorator==3.4.0',
-      		 'django-extensions==1.3.3',
-      		 'django-picklefield==0.3.1',
-      		 'docutils==0.10',
-     	   	 'drmaa==0.7.6',
-      		 'ipython==0.13.1',
-      		 'networkx==1.7',
-      		 'pygraphviz==1.1',
-      		 'six==1.2.0',
-      		 'wsgiref==0.1.2',
-      		 'ordereddict',
-      		 'ipdb==0.8'
-  	 ]
-   else:
-	install_requires=[
-      		 'distribute>=0.6.28',
-      		 'Django==1.4.3',
-      		 'MySQL-python==1.2.4',
-      		 'argparse==1.2.1',
-      		 'configobj==4.7.2',
-      		 'decorator==3.4.0',
-      		 'django-extensions==1.0.3',
-      		 'django-picklefield==0.3.0',
-      		 'docutils==0.10',
-      		 'drmaa==0.5',
-      		 'ipython==0.13.1',
-      		 'networkx==1.7',
-      		 'pygraphviz==1.1',
-      		 'six==1.2.0',
-      		 'wsgiref==0.1.2',
-      		 'ordereddict',
-      		 'ipdb==0.8'
-       ]
+    install_requires=install_requires
 )
